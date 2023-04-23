a0765ce8de5ef2cf37a2c840c2f6c803206d73e6
swapnil shirish janrao <73775658+sj480@users.noreply.github.com>
2023-04-23 15:50:34 +0530

    Add files via upload

---
+++ Palindrome_or_Not.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+    int number;
+    cout<<"Enter a positive integer: ";
+    cin>>number; 
+
+    int mainvalue = number; 
+    int lastdigit; 
+    int reverse = 0; 
+
+
+    while(mainvalue > 0)
+    {
+
+        lastdigit = mainvalue % 10; 
+        reverse = reverse * 10 + lastdigit;
+        mainvalue = mainvalue/10; 
+
+    }
+
+   if(number == reverse)
+   {
+    cout<<number<<"is a palindrome number."<<endl;
+   }
+   else
+   {
+    cout<<number<<" is not a palindrome number."<<endl;
+   }
+
+    return 0; 
+}
