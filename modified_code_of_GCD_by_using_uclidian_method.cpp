b3c9c4ff60eee2d13228825777cfe6ba86801b8a
swapnil shirish janrao <73775658+sj480@users.noreply.github.com>
2023-04-25 22:07:53 +0530

    Add files via upload

---
+++ modified_code_of_GCD_by_using_uclidian_method.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int a,b; 
+    cin>>a>>b; 
+
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a = a%b; 
+        }
+        else
+        {
+            b = b%a; 
+        }
+        cout<<"gcd ( " <<a<< ","<<b<< " )"<<endl;
+    }
+
+    return 0; 
+}
